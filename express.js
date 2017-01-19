1e9d099a94dbba512686aeb9a07e328572d1454c
i5ting <i5ting@126.com>
2017-01-20 02:52:27 +0800

    refact, please ignore

--- express.js
+++ express.js
@@ -1,17 +1,10 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 app.get('/', function (req, res) {
-  //   res.send('hello ');
-  //   res.send(' world');
-  res.write('hello');
-  res.write('<br>dodo');
-  res.end();
-});
+  res.write('hello')
+  res.write('<br>dodo')
+  res.end()
+})
 
-
-module.exports = function () {
-  app.listen(9091);
-
-  console.log('server on 9091');
-};
+app.listen(9091)
--- http.js
+++ http.js
@@ -1,43 +1,41 @@
-var http = require('http');
+var http = require('http')
 
 var app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html', 'charset': 'utf-8' });
+    res.writeHead(200, { 'Content-Type': 'text/html', 'charset': 'utf-8' })
 
-    res.write('<br>loading...');
-    timer(5, res);
-});
+    res.write('<br>loading...')
+    timer(5, res)
+})
 
-
-
-var isEnd = false;
+var isEnd = false
 /** 生成倒计时渲染 */
 function timer(num, res) {
-    isEnd = false;
-    var t = Math.floor(Math.random() * 10) * 3000;
+    isEnd = false
+    var t = Math.floor(Math.random() * 10) * 3000
 
     setTimeout(function () {
         if (isEnd) {
-            return;
+            return
         }
         if (num == 1) {
-            isEnd = true;
-            res.end(`<div>last timer: ${t}ms</div>`);
+            isEnd = true
+            res.end(`<div>last timer: ${t}ms</div>`)
         } else {
-            res.write(`<div>timer${num} : ${t}ms</div>`);
+            res.write(`<div>timer${num} : ${t}ms</div>`)
         }
-    }, t);
+    }, t)
     if (num > 1) {
-        timer(num - 1, res);
+        timer(num - 1, res)
     }
 }
 
 /** 异常处理 */
 process.on('uncaughtException', function (err) {
-    console.log(err);
-});
+    console.log(err)
+})
 
 module.exports = function () {
-    app.listen(9090);
+    app.listen(9090)
 
-    console.log('server on 9090');
-};
+    console.log('server on 9090')
+}
