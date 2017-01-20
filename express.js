05f4ad1feee7ee8bc2c36a1d7da8098c4bab6ae7
i5ting <i5ting@126.com>
2017-01-20 19:22:12 +0800

    lazycommmit, please ignore

--- express.js
+++ express.js
@@ -1,10 +1,23 @@
+'use strict'
+
+const sleep = ms => new Promise(r => setTimeout(r, ms))
+
 var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.write('hello')
-  res.write('<br>dodo')
-  res.end()
+  res.type('html');   
+  res.write('loading...<br>')
+  
+  return sleep(2000).then(function() {
+    res.write(`timer: 2000ms<br>`)
+    return sleep(5000)
+  })
+  .then(function() {
+    res.write(`timer: 5000ms<br>`)
+  }).then(function() {
+    res.end()
+  })
 })
 
-app.listen(9091)
+app.listen(3000)
--- koa.js
+++ koa.js
@@ -6,26 +6,24 @@ const Readable = require('stream').Readable
 const sleep = ms => new Promise(r => setTimeout(r, ms))
 
 app.use(function* () {
-    /** 必须 */
-    const view = new Readable()
-    view._read = () => { }
+  const view = new Readable()
+  view._read = () => { }
 
-    this.body = view
-    this.type = 'html'
-    this.status = 200
+  this.body = view
+  this.type = 'html'
+  this.status = 200
 
-    view.push('loading...<br>')
+  view.push('loading...<br>')
 
-    co(function* () {
-        yield sleep(2000)
-        view.push(`timer: 2000ms<br>`)
-        yield sleep(5000)
-        view.push(`timer: 5000ms<br>`)
+  co(function* () {
+      yield sleep(2000)
+      view.push(`timer: 2000ms<br>`)
+      yield sleep(5000)
+      view.push(`timer: 5000ms<br>`)
 
-        /** 结束传送 */
-        view.push(null)
-    }).catch(e => { })
-
+      /** 结束传送 */
+      view.push(null)
+  }).catch(e => { })
 })
 
 app.listen(9092)
